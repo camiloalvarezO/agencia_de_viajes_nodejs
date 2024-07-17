0f5ff6b503fd7ad5781c3b1484e2e24cae4d0a34
camiloalvarezO <camiloandresalvarez@unicesar.edu.co>
2024-07-16 23:55:34 -0500

    agregué conexión a la base de datos mysql

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
 
 import express from "express";
 import router from "./routes/index.js"
+import db from "./config/db.js";
 const app = express();
 
+//conexión a la base de datos mysql
+db.authenticate()
+    .then(response => console.log("base de datos conectada"))
+    .catch( error => console.log("error en la conexión a la base de datos"))
+    
 const port = process.env.PORT || 4000;
 // establecer public como carpeta publica para el usuario
 // agregando la carpeta public como archivos estáticos de express
+
+app.use( (req,res,next)=>{
+    const year = new Date();
+
+    const fechaActual = year.getFullYear();
+    res.locals.fechaActual= fechaActual;
+    res.locals.nombresitio= "agencia de viajes" 
+    // res.locals.nuevaVariable = "nueva variable"
+    // console.log(res.locals.nuevaVariable);
+    // se puede colocar next sin return pero si se queda trabado ponerle return
+    return next();
+})
+
 app.use(express.static('public'))
 //habilitar pug
 app.set('view engine','pug')
--- routes/index.js
+++ routes/index.js
@@ -3,17 +3,29 @@ import express from 'express';
 const router = express.Router();
 
 router.get('/',(req,res) => {
-    res.render('inicio')
+    res.render('inicio',{
+        "pagina" : "Inicio"
+    })
+})
+
+router.get('/viajes',(req,res) => {
+    res.render('viajes',{
+        "pagina" : "Viajes"
+    })
+})
+
+router.get('/testimoniales',(req,res) => {
+    res.render('testimoniales',{
+        "pagina" : "Testimoniales"
+    })
 })
 router.get('/nosotros', (req,res) =>{
 
     const viajes = 'viaje a san juan'
     // solo con colocar el nombre del archivo escaneará el que tenga el .pug
     res.render('nosotros',{ // recordar que esto aqui para mandar como segundo parametro es un objeto
-        viajes
+        viajes,
+        "pagina": "Nosotros"
     }) 
 })
-
-
-
 export default router;
